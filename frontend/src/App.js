8e69c4b3cc21f371a7ef79f431f2e8f7627235ec
harshvardhan singh <harshvardhansingh458@protonmail.com>
2024-02-01 23:18:14 +0530

    profile page route update now contains username as parameter

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,7 +12,7 @@ function App() {
         <Routes>
           <Route index path="/" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
-          <Route path="/profile" element={<Profile />} />
+          <Route path="/profile/:username" element={<Profile />} />
           <Route path="/editProfile" element={<EditProfile />} />
           <Route path="/timeline" element={<Timeline />} />
         </Routes>
