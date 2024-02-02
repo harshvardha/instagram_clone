9c108d462ed0274aedf518acb905bc0265052bcc
harshvardhan singh <harshvardhansingh458@protonmail.com>
2024-02-02 23:09:55 +0530

    added a timeago.js file to show the time elapsed for a post or comment etc...

---
+++ frontend/src/utils/timeAgo.js
@@ -0,0 +1,21 @@
+export const timeAgo = (timestamp) => {
+    const now = Date.now();
+    const timeStamp = new Date(timestamp).getTime();
+    const secondsAgo = Math.floor((now - timeStamp) / 1000);
+
+    if (secondsAgo < 60) {
+        return `${secondsAgo}s`;
+    } else if (secondsAgo < 3600) {
+        const minutesAgo = Math.floor(secondsAgo / 60);
+        return `${minutesAgo}m`;
+    } else if (secondsAgo < 86400) {
+        const hoursAgo = Math.floor(secondsAgo / 3600);
+        return `${hoursAgo}h`;
+    } else if (secondsAgo < 604800) {
+        const daysAgo = Math.floor(secondsAgo / 86400);
+        return `${daysAgo}d`;
+    } else {
+        const weeksAgo = Math.floor(secondsAgo / 604800); // 7 days in seconds
+        return `${weeksAgo}w`;
+    }
+}
